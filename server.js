7192d68786fbd2d64a300a66030db6e9bf8c3700
konankikesava <konankik@hotmail.com>
2020-12-13 08:34:26 +0200

    deployment changes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var connect = require('connect'),
-serveStatic = require('serve-static');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-var app = connect();
+const app = express();
 
-app.use(serveStatic("dist/petmanager-ui-master"))
-app.listen(process.env.PORT || 80);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/petmanager'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/petmanager/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
