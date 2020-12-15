4ccc33b8fa6ddba88ad9757ce2984f40885edfc5
konankikesava <konankik@hotmail.com>
2020-12-15 14:21:10 +0200

    final changes1

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/petmanager'));
+app.use(express.static(__dirname + '/dist/petmanager-ui-master'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/petmanager/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/petmanager-ui-master/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
